11e834aa29ce5b593356a7870d6f1890fed1e53c
Maksimka <138521714+04Maksimka@users.noreply.github.com>
2024-11-24 11:51:58 +0300

    Update 100_prosoners_and_100_boxes.py

--- paradoxes of probobility theory/100_prosoners_and_100_boxes.py
+++ paradoxes of probobility theory/100_prosoners_and_100_boxes.py
@@ -1,28 +1,41 @@
+"""
+The 100 Prisoners Problem. 
+Veritasium's video : https://www.youtube.com/watch?v=iSNsgj1OCLA
+Wiki : https://en.wikipedia.org/wiki/100_prisoners_problem
+"""
 import random
 
-def each_prisoner_success(prisoner_id, _boxes):
-    
+NUM_OF_PRISONERS = 100
+ITERATIONS = 1000
 
+
+def each_prisoner_success(prisoner_id, _boxes):
+    """Gets True if prisoner escapes."""
     current_box = prisoner_id
-    for _ in range(50):
+    for _ in range(NUM_OF_PRISONERS // 2):
         if _boxes[current_box - 1] == prisoner_id:
             return True
         current_box = _boxes[current_box - 1]
     return False
 
+
 def all_prisoners_success(_boxes):
+    """Gets True if all prisoners escape."""
     success = True
-    for prisoner_id in range(1, 100 + 1):
+    for prisoner_id in range(1, NUM_OF_PRISONERS + 1):
         success = success & each_prisoner_success(prisoner_id, _boxes)
     return success
 
 
-prisoners_escaped = 0
-for _ in range(100):
-    boxes = [i for i in range(1, 100 + 1)]
-    random.shuffle(boxes)
-    if all_prisoners_success(boxes):
-        prisoners_escaped += 1
+def prisoners_100_probability():
+    prisoners_escaped = 0
+    for _ in range(ITERATIONS):
+        boxes = [i for i in range(1, NUM_OF_PRISONERS + 1)]
+        random.shuffle(boxes)
+        if all_prisoners_success(boxes):
+            prisoners_escaped += 1
+    return prisoners_escaped / ITERATIONS
 
-print(prisoners_escaped / 100)
 
+if __name__ == '__main__':
+    print(prisoners_100_probability())
